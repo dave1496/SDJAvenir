cc6475fc6b137b0dda7bea1e5d70f5604ef469b6
Dave Mamik <david.el-malih@student.ecp.fr>
2018-06-10 18:50:36 +0200

    Feature: init stripe

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -21,22 +21,28 @@ class Home extends Component {
 
 		this.state = {
 			amount: 0,
+			email: "",
 			loading: true,
 		};
 
 		this.addAmount = this.addAmount.bind(this);
 		this.resetAmount = this.resetAmount.bind(this);
 		this.editAmount = this.editAmount.bind(this);
+		this.appleProcessPayment = this.appleProcessPayment.bind(this);
+		this.androidProcessPayment = this.androidProcessPayment.bind(this);
+		this.cardPayment = this.cardPayment.bind(this);
 		this.processPayment = this.processPayment.bind(this);
 		this.showError = this.showError.bind(this);
 	}
 
 	UNSAFE_componentWillMount() {
 		const { uid } = firebase.auth().currentUser;
-		firebase.database().ref(`users/${uid}`).once('value').then(snapshot => {
+		firebase.database().ref(`users/${uid}`).on('value', snapshot => {
+			const user = snapshot.val() ? snapshot.val() : {};
 			this.setState({
-			amount: snapshot.val().amount,
-			loading: false,
+				amount: user.amount,
+				email: user.email,
+				loading: false,
 			});
 		});
 		stripe.setOptions({
@@ -61,8 +67,44 @@ class Home extends Component {
 		firebase.database().ref(`users/${uid}`).update({ amount });
 	}
 
+	appleProcessPayment() {
+		stripe.deviceSupportsApplePay().then(supportsApplePay => {
+			if (supportsApplePay) {
+				stripe.canMakeApplePayPayments(['american_express', 'discover', 'master_card', 'visa']).then(canMakePayments => {
+					if (canMakePayments) {
+						// stripe.paymentRequestWithApplePay();
+					} else {
+						this.cardPayment();
+					}
+				}).catch(() => this.showError("An error has occured"));
+			} else {
+				this.cardPayment();
+			}
+		}).catch(() => this.showError("An error has occured"));
+	}
+
+	androidProcessPayment() {
+		this.cardPayment();
+	}
+
+	cardPayment() {
+		const options = {
+			managedAccountCurrency: "EUR",
+			prefilledInformation: {
+				email: this.state.email,
+			},
+		}
+		stripe.paymentRequestWithCardForm(options).then(token => {
+			alert(JSON.stringify(token));
+		}).catch(() => this.showError("An error has occured"));
+	}
+
 	processPayment() {
-		
+		if (Platform.OS === 'ios') {
+			this.appleProcessPayment();
+		} else {
+			this.androidProcessPayment();
+		}
 	}
 
 	showError(msg) {
